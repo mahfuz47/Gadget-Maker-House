71c52b2c8791427d4bf3bc92b2402746de0a1488
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-26 22:36:40 +0600

    make admin route ui done

--- src/Pages/Dashboard/MakeAdmin.js
+++ src/Pages/Dashboard/MakeAdmin.js
@@ -1,9 +1,50 @@
 import React from "react";
+import { useQuery } from "react-query";
+import Loading from "../../Utilities/Loading";
+import UserRow from "./UserRow";
 
 const MakeAdmin = () => {
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery("users", () =>
+    fetch("http://localhost:5000/users", {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
-      <h2>make</h2>
+      <h2 className="text-2xl">All Users {users.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <UserRow
+                key={user._id}
+                user={user}
+                serial={1}
+                refetch={refetch}
+              ></UserRow>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
