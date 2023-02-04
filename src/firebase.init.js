08e1650cd7d57cfdbfa2c326272f445372376a31
Mahfuzur Rahman <mahfuzshikder560@gmail.com>
2023-02-04 19:39:39 +0600

    fixing firebase issues

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -4,15 +4,13 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_apiKey}`,
-  authDomain: `${process.env.REACT_APP_authDomain}`,
-  projectId: `${process.env.REACT_APP_projectId}`,
-  storageBucket: `${process.env.REACT_APP_storageBucket}`,
-  messagingSenderId: `${process.env.REACT_APP_messagingSenderId}`,
-  appId: `${process.env.REACT_APP_appId}`,
-  measurementId: `${process.env.REACT_APP_measurementId}`,
+  apiKey: process.env.local.REACT_APP_apiKey,
+  authDomain: process.env.local.REACT_APP_authDomain,
+  projectId: process.env.local.REACT_APP_projectId,
+  storageBucket: process.env.local.REACT_APP_storageBucket,
+  messagingSenderId: process.env.local.REACT_APP_messagingSenderId,
+  appId: process.env.local.REACT_APP_appId,
 };
 
 // Initialize Firebase
