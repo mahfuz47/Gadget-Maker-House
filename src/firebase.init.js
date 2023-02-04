fcd89cf0f1bc7624d92be3cf5ca60a184fefc5fc
Mahfuzur Rahman <mahfuzshikder560@gmail.com>
2023-02-04 19:33:46 +0600

    firebase issues fixing

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC7MH-urRkPTMTsI2clwKCSRYchENYlIgc",
-  authDomain: "gadget-house-app.firebaseapp.com",
-  projectId: "gadget-house-app",
-  storageBucket: "gadget-house-app.appspot.com",
-  messagingSenderId: "816471626195",
-  appId: "1:816471626195:web:180655fe2bcb41a1e09f26",
+  apiKey: `${process.env.REACT_APP_apiKey}`,
+  authDomain: `${process.env.REACT_APP_authDomain}`,
+  projectId: `${process.env.REACT_APP_projectId}`,
+  storageBucket: `${process.env.REACT_APP_storageBucket}`,
+  messagingSenderId: `${process.env.REACT_APP_messagingSenderId}`,
+  appId: `${process.env.REACT_APP_appId}`,
+  measurementId: `${process.env.REACT_APP_measurementId}`,
 };
 
 // Initialize Firebase
